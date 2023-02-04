e2ef3cad29de641a3e378e78e3d97683c7b75e11
Krishna <krishnarathi341@gmail.com>
2023-02-04 15:01:00 +0530

    completed final reponsive rendering of News Card now only data is needed to be feeded

--- src/Components/NewsContainer/NewsContainer.js
+++ src/Components/NewsContainer/NewsContainer.js
@@ -2,11 +2,31 @@ import "./NewsContainer.css";
 import Container from "@mui/material/Container";
 import Appdownload from "./Appdownload";
 import NewsCard from "./NewsCard";
+import { useEffect, useState } from "react";
 
 const NewsContainer = () => {
+  const [maxWidthValue, setMaxWidthValue] = useState("md");
+
+  useEffect(() => {
+    function consoleResize() {
+      if (window.innerWidth < 870) {
+        setMaxWidthValue("sm");
+      }
+
+      if (window.innerWidth >= 870) {
+        setMaxWidthValue("md");
+      }
+    }
+
+    window.addEventListener("resize", consoleResize);
+    return () => {
+      window.removeEventListener("resize", consoleResize);
+    };
+  });
+
   return (
     <>
-      <Container maxWidth="md">
+      <Container maxWidth={maxWidthValue} className="container">
         <Appdownload />
         <NewsCard />
       </Container>
